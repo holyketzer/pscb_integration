38ad5ae415804c152a01a0ff2561bb6df9511a97
Alex Emelyanov <holyketzer@gmail.com>
2019-02-16 23:46:46 +0300

    Add timeouts to Farady client

--- lib/pscb_integration/config.rb
+++ lib/pscb_integration/config.rb
@@ -1,11 +1,14 @@
 module PscbIntegration
   class Config
-    attr_accessor :host, :market_place, :secret_key, :demo_secret_key, :confirm_payment_callback
+    attr_accessor :host, :market_place, :secret_key, :demo_secret_key, :confirm_payment_callback,
+      :timeout
 
     def initialize(attrs = {})
       attrs.each do |name, value|
         public_send("#{name}=", value)
       end
+
+      self.timeout ||= 5
     end
   end
 end
